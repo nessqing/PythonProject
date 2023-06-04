062215350926e4c349a233f901b2da40289bfa72
peiq <88368889+nessqing@users.noreply.github.com>
2023-06-05 00:26:54 +0800

    Python society
    1.reply pop() method question
    -> not define it will remove last item in list

--- exam.py
+++ exam.py
@@ -1,20 +1,31 @@
-def test_exc_return():
-    try:
-        print('in try....')
-        raise KeyError()
-        return 1
-
-    except KeyError:
-        print('key error')
-        return 2
-
-    else:
-        return 3
-
-    finally:
-        print('finally')
-
-
-if __name__ == "__main__":
-    a = test_exc_return()
-    print(a)
+# def test_exc_return():
+#     try:
+#         print('in try....')
+#         raise KeyError()
+#         return 1
+#
+#     except KeyError:
+#         print('key error')
+#         return 2
+#
+#     else:
+#         return 3
+#
+#     finally:
+#         print('finally')
+#
+#
+# if __name__ == "__main__":
+#     a = test_exc_return()
+#     print(a)
+a =['foo','aa','baz','qux','corge']
+print(len(a))
+while a:
+    if len(a) < 5:
+        break
+    print(a.pop())
+# print('done')
+# a.pop(0)
+# a.pop(1)
+# print(a.pop(0))
+# print(len(a))
